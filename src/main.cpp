024bb2622428376b2e3c542aa505ae33d7d3cc07
Tijana Zivkovic <tijanazivkovic99@gmail.com>
2024-04-09 20:38:19 +0200

    Added walls with textures. Enabled face culling.

--- src/main.cpp
+++ src/main.cpp
@@ -24,7 +24,7 @@ const unsigned int SCR_WIDTH = 1200;
 const unsigned int SCR_HEIGHT = 800;
 
 // camera
-Camera camera(glm::vec3(0.0f, 2.0f, 6.0f));
+Camera camera(glm::vec3(0.0f, 1.0f, 6.0f));
 float lastX = SCR_WIDTH / 2.0f;
 float lastY = SCR_HEIGHT / 2.0f;
 bool firstMouse = true;
@@ -75,51 +75,102 @@ int main() {
     // build and compile shaders
     Shader platform1Shader("resources/shaders/platform1.vs", "resources/shaders/platform1.fs");
     Shader platform2Shader("resources/shaders/platform2.vs", "resources/shaders/platform2.fs");
+    Shader wall1Shader("resources/shaders/wall1.vs", "resources/shaders/wall1.fs" );
+    Shader wall2Shader("resources/shaders/wall2.vs", "resources/shaders/wall2.fs" );
+
 
     // basic cube vertices - to be used for drawing platforms
-    float vertices[] = {
-            // positions          // normals           // texture coords
-            -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f,  0.0f,
-            0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  4.0f,  0.0f,
-            0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  4.0f,  4.0f,
-            0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  4.0f,  4.0f,
-            -0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f,  4.0f,
-            -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f,  0.0f,
+    float platformVertices[] = {
+            // positions                // normals              //texture coords
+            // back face (CCW winding)
+            0.5f, -0.5f, -0.5f, 0.0f,  0.0f, -1.0f,  0.0f, 0.0f, // bottom-left
+            -0.5f, -0.5f, -0.5f, 0.0f,  0.0f, -1.0f, 4.0f, 0.0f, // bottom-right
+            -0.5f,  0.5f, -0.5f, 0.0f,  0.0f, -1.0f, 4.0f, 4.0f, // top-right
+            -0.5f,  0.5f, -0.5f, 0.0f,  0.0f, -1.0f, 4.0f, 4.0f, // top-right
+            0.5f,  0.5f, -0.5f, 0.0f,  0.0f, -1.0f, 0.0f, 4.0f, // top-left
+            0.5f, -0.5f, -0.5f, 0.0f,  0.0f, -1.0f, 0.0f, 0.0f, // bottom-left
+            // front face (CCW winding)
+            -0.5f, -0.5f,  0.5f, 0.0f,  0.0f,  1.0f,  0.0f, 0.0f, // bottom-left
+            0.5f, -0.5f,  0.5f, 0.0f,  0.0f,  1.0f, 4.0f, 0.0f, // bottom-right
+            0.5f,  0.5f,  0.5f, 0.0f,  0.0f,  1.0f, 4.0f, 4.0f, // top-right
+            0.5f,  0.5f,  0.5f, 0.0f,  0.0f,  1.0f, 4.0f, 4.0f, // top-right
+            -0.5f,  0.5f,  0.5f, 0.0f,  0.0f,  1.0f, 0.0f, 4.0f, // top-left
+            -0.5f, -0.5f,  0.5f, 0.0f,  0.0f,  1.0f, 0.0f, 0.0f, // bottom-left
+            // left face (CCW)
+            -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f, 0.0f, 0.0f, // bottom-left
+            -0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f, 4.0f, 0.0f, // bottom-right
+            -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f, 4.0f, 4.0f, // top-right
+            -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f, 4.0f, 4.0f, // top-right
+            -0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f, 0.0f, 4.0f, // top-left
+            -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f, 0.0f, 0.0f, // bottom-left
+            // right face (CCW)
+            0.5f, -0.5f,  0.5f, 1.0f,  0.0f,  0.0f, 0.0f, 0.0f, // bottom-left
+            0.5f, -0.5f, -0.5f, 1.0f,  0.0f,  0.0f, 4.0f, 0.0f, // bottom-right
+            0.5f,  0.5f, -0.5f, 1.0f,  0.0f,  0.0f, 4.0f, 4.0f, // top-right
+            0.5f,  0.5f, -0.5f, 1.0f,  0.0f,  0.0f, 4.0f, 4.0f, // top-right
+            0.5f,  0.5f,  0.5f, 1.0f,  0.0f,  0.0f, 0.0f, 4.0f, // top-left
+            0.5f, -0.5f,  0.5f, 1.0f,  0.0f,  0.0f, 0.0f, 0.0f, // bottom-left
+            // bottom face (CCW)
+            -0.5f, -0.5f, -0.5f, 0.0f, -1.0f,  0.0f, 0.0f, 0.0f, // bottom-left
+            0.5f, -0.5f, -0.5f, 0.0f, -1.0f,  0.0f, 4.0f, 0.0f, // bottom-right
+            0.5f, -0.5f,  0.5f, 0.0f, -1.0f,  0.0f, 4.0f, 4.0f, // top-right
+            0.5f, -0.5f,  0.5f, 0.0f, -1.0f,  0.0f, 4.0f, 4.0f, // top-right
+            -0.5f, -0.5f,  0.5f, 0.0f, -1.0f,  0.0f, 0.0f, 4.0f, // top-left
+            -0.5f, -0.5f, -0.5f, 0.0f, -1.0f,  0.0f, 0.0f, 0.0f, // bottom-left
+            // top face (CCW)
+            -0.5f,  0.5f,  0.5f, 0.0f,  1.0f,  0.0f, 0.0f, 0.0f, // bottom-left
+            0.5f,  0.5f,  0.5f, 0.0f,  1.0f,  0.0f, 4.0f, 0.0f, // bottom-right
+            0.5f,  0.5f, -0.5f, 0.0f,  1.0f,  0.0f, 4.0f, 4.0f, // top-right
+            0.5f,  0.5f, -0.5f, 0.0f,  1.0f,  0.0f, 4.0f, 4.0f, // top-right
+            -0.5f,  0.5f, -0.5f, 0.0f,  1.0f,  0.0f, 0.0f, 4.0f, // top-left
+            -0.5f,  0.5f,  0.5f, 0.0f,  1.0f,  0.0f, 0.0f, 0.0f, // bottom-left
+    };
 
-            -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  0.0f,  0.0f,
-            0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  4.0f,  0.0f,
-            0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  4.0f,  4.0f,
-            0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  4.0f,  4.0f,
-            -0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  0.0f,  4.0f,
-            -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  0.0f,  0.0f,
-
-            -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  4.0f,  0.0f,
-            -0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  4.0f,  4.0f,
-            -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f,  4.0f,
-            -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f,  4.0f,
-            -0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  0.0f,  0.0f,
-            -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  4.0f,  0.0f,
-
-            0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  4.0f,  0.0f,
-            0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  4.0f,  4.0f,
-            0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  0.0f,  4.0f,
-            0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  0.0f,  4.0f,
-            0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  0.0f,  0.0f,
-            0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  4.0f,  0.0f,
-
-            -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f,  4.0f,
-            0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  4.0f,  4.0f,
-            0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  4.0f,  0.0f,
-            0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  4.0f,  0.0f,
-            -0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  0.0f,  0.0f,
-            -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f,  4.0f,
-
-            -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f,  4.0f,
-            0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  4.0f,  4.0f,
-            0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  4.0f,  0.0f,
-            0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  4.0f,  0.0f,
-            -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  0.0f,  0.0f,
-            -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f,  4.0f
+    // basic cube vertices - to be used for drawing platforms
+    float wallVertices[] = {
+            // positions                // normals              //texture coords
+            // back face (CCW winding)
+            0.5f, -0.5f, -0.5f, 0.0f,  0.0f, -1.0f,  0.0f, 0.0f, // bottom-left
+            -0.5f, -0.5f, -0.5f, 0.0f,  0.0f, -1.0f, 2.0f, 0.0f, // bottom-right
+            -0.5f,  0.5f, -0.5f, 0.0f,  0.0f, -1.0f, 2.0f, 2.0f, // top-right
+            -0.5f,  0.5f, -0.5f, 0.0f,  0.0f, -1.0f, 2.0f, 2.0f, // top-right
+            0.5f,  0.5f, -0.5f, 0.0f,  0.0f, -1.0f, 0.0f, 2.0f, // top-left
+            0.5f, -0.5f, -0.5f, 0.0f,  0.0f, -1.0f, 0.0f, 0.0f, // bottom-left
+            // front face (CCW winding)
+            -0.5f, -0.5f,  0.5f, 0.0f,  0.0f,  1.0f,  0.0f, 0.0f, // bottom-left
+            0.5f, -0.5f,  0.5f, 0.0f,  0.0f,  1.0f, 2.0f, 0.0f, // bottom-right
+            0.5f,  0.5f,  0.5f, 0.0f,  0.0f,  1.0f, 2.0f, 2.0f, // top-right
+            0.5f,  0.5f,  0.5f, 0.0f,  0.0f,  1.0f, 2.0f, 2.0f, // top-right
+            -0.5f,  0.5f,  0.5f, 0.0f,  0.0f,  1.0f, 0.0f, 2.0f, // top-left
+            -0.5f, -0.5f,  0.5f, 0.0f,  0.0f,  1.0f, 0.0f, 0.0f, // bottom-left
+            // left face (CCW)
+            -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f, 0.0f, 0.0f, // bottom-left
+            -0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f, 2.0f, 0.0f, // bottom-right
+            -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f, 2.0f, 2.0f, // top-right
+            -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f, 2.0f, 2.0f, // top-right
+            -0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f, 0.0f, 2.0f, // top-left
+            -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f, 0.0f, 0.0f, // bottom-left
+            // right face (CCW)
+            0.5f, -0.5f,  0.5f, 1.0f,  0.0f,  0.0f, 0.0f, 0.0f, // bottom-left
+            0.5f, -0.5f, -0.5f, 1.0f,  0.0f,  0.0f, 2.0f, 0.0f, // bottom-right
+            0.5f,  0.5f, -0.5f, 1.0f,  0.0f,  0.0f, 2.0f, 2.0f, // top-right
+            0.5f,  0.5f, -0.5f, 1.0f,  0.0f,  0.0f, 2.0f, 2.0f, // top-right
+            0.5f,  0.5f,  0.5f, 1.0f,  0.0f,  0.0f, 0.0f, 2.0f, // top-left
+            0.5f, -0.5f,  0.5f, 1.0f,  0.0f,  0.0f, 0.0f, 0.0f, // bottom-left
+            // bottom face (CCW)
+            -0.5f, -0.5f, -0.5f, 0.0f, -1.0f,  0.0f, 0.0f, 0.0f, // bottom-left
+            0.5f, -0.5f, -0.5f, 0.0f, -1.0f,  0.0f, 2.0f, 0.0f, // bottom-right
+            0.5f, -0.5f,  0.5f, 0.0f, -1.0f,  0.0f, 2.0f, 2.0f, // top-right
+            0.5f, -0.5f,  0.5f, 0.0f, -1.0f,  0.0f, 2.0f, 2.0f, // top-right
+            -0.5f, -0.5f,  0.5f, 0.0f, -1.0f,  0.0f, 0.0f, 2.0f, // top-left
+            -0.5f, -0.5f, -0.5f, 0.0f, -1.0f,  0.0f, 0.0f, 0.0f, // bottom-left
+            // top face (CCW)
+            -0.5f,  0.5f,  0.5f, 0.0f,  1.0f,  0.0f, 0.0f, 0.0f, // bottom-left
+            0.5f,  0.5f,  0.5f, 0.0f,  1.0f,  0.0f, 2.0f, 0.0f, // bottom-right
+            0.5f,  0.5f, -0.5f, 0.0f,  1.0f,  0.0f, 2.0f, 2.0f, // top-right
+            0.5f,  0.5f, -0.5f, 0.0f,  1.0f,  0.0f, 2.0f, 2.0f, // top-right
+            -0.5f,  0.5f, -0.5f, 0.0f,  1.0f,  0.0f, 0.0f, 2.0f, // top-left
+            -0.5f,  0.5f,  0.5f, 0.0f,  1.0f,  0.0f, 0.0f, 0.0f, // bottom-left
     };
 
     // platform positions
@@ -128,14 +179,37 @@ int main() {
             glm::vec3( 1.5f, 0.0f, 0.5f)
     };
 
-    unsigned int VBO, platformVAO;
-    glGenVertexArrays(1, &platformVAO);
-    glGenBuffers(1, &VBO);
+    // wall positions
+    glm::vec3 wallPositions[] = {
+            glm::vec3( -3.0f,  1.625f,  -2.525f),
+            glm::vec3( -5.425f, 1.625f, -1.2f),
+            glm::vec3( 2.5f, 1.125f, -2.025f),
+            glm::vec3( 3.925f, 1.125f, 0.05f)
+    };
 
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    unsigned int platformVBO, platformVAO;
+    glGenVertexArrays(1, &platformVAO);
+    glGenBuffers(1, &platformVBO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, platformVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(platformVertices), platformVertices, GL_STATIC_DRAW);
 
     glBindVertexArray(platformVAO);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+
+    unsigned int wallVBO, wallVAO;
+    glGenVertexArrays(1, &wallVAO);
+    glGenBuffers(1, &wallVBO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, wallVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(wallVertices), wallVertices, GL_STATIC_DRAW);
+
+    glBindVertexArray(wallVAO);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
@@ -148,6 +222,12 @@ int main() {
     unsigned int specularMapPlatform1 = loadTexture("resources/textures/WoodFlooringAshSuperWhite_specular.jpg");
     unsigned int diffuseMapPlatform2 = loadTexture("resources/textures/TilesBlackSlateSquare_diffuse.png");
     unsigned int specularMapPlatform2 = loadTexture("resources/textures/TilesBlackSlateSquare_specular.png");
+    unsigned int diffuseMapWall1 = loadTexture("resources/textures/BricksReclaimedWhitewashedOffset_diffuse.png");
+    unsigned int specularMapWall1 = loadTexture("resources/textures/BricksReclaimedWhitewashedOffset_specular.png");
+    unsigned int diffuseMapWall2 = loadTexture("resources/textures/StuccoRoughCast2_diffuse.png");
+    unsigned int specularMapWall2 = loadTexture("resources/textures/StuccoRoughCast_specular.png");
+
+
 
     // shader configuration
     platform1Shader.use();
@@ -158,9 +238,16 @@ int main() {
     platform2Shader.setInt("material.diffuse", 2);
     platform2Shader.setInt("material.specular", 3);
 
+    wall1Shader.use();
+    wall1Shader.setInt("material.diffuse", 4);
+    wall1Shader.setInt("material.specular", 5);
+
+    wall2Shader.use();
+    wall2Shader.setInt("material.diffuse", 6);
+    wall2Shader.setInt("material.specular", 7);
 
     // directional light settings
-    glm::vec3 direction = glm::vec3(-0.6f, -5.0f, 3.0f);
+    glm::vec3 direction = glm::vec3(0.0f, -4.0f, -5.0f);
     glm::vec3 dirLightAmbient = glm::vec3(0.05f, 0.05f, 0.05f);
     glm::vec3 dirLightDiffuse = glm::vec3(0.4f, 0.4f, 0.4f);
     glm::vec3 dirLightSpecular = glm::vec3(0.5f, 0.5f, 0.5f);
@@ -188,6 +275,11 @@ int main() {
                                                 (float) SCR_WIDTH / (float) SCR_HEIGHT, 0.1f, 100.0f);
         glm::mat4 view = camera.GetViewMatrix();
         glm::mat4 model = glm::mat4(1.0f);
+
+        // enabling face culling for platforms and walls
+        glEnable(GL_CULL_FACE);
+        glFrontFace(GL_CCW);
+        glCullFace(GL_BACK);
 
         // =========================================== draw platforms ===========================================
 
@@ -254,6 +346,99 @@ int main() {
 
         glDrawArrays(GL_TRIANGLES, 0, 36);
 
+        // =========================================== platforms drawn ===========================================
+
+        // =========================================== draw walls ================================================
+
+        // bind diffuse map
+        glActiveTexture(GL_TEXTURE4);
+        glBindTexture(GL_TEXTURE_2D, diffuseMapWall1);
+        // bind specular map
+        glActiveTexture(GL_TEXTURE5);
+        glBindTexture(GL_TEXTURE_2D, specularMapWall1);
+        // bind diffuse map
+        glActiveTexture(GL_TEXTURE6);
+        glBindTexture(GL_TEXTURE_2D, diffuseMapWall2);
+        // bind specular map
+        glActiveTexture(GL_TEXTURE7);
+        glBindTexture(GL_TEXTURE_2D, specularMapWall2);
+
+        glBindVertexArray(wallVAO);
+
+        // ------------------------------------------- 1st wall --------------------------------------------------
+        wall1Shader.use();
+
+        wall1Shader.setVec3("viewPos", camera.Position);
+        wall1Shader.setFloat("material.shininess", 32.0f);
+
+        // directional light
+        wall1Shader.setVec3("dirLight.direction", direction);
+        wall1Shader.setVec3("dirLight.ambient", dirLightAmbient);
+        wall1Shader.setVec3("dirLight.diffuse", dirLightDiffuse);
+        wall1Shader.setVec3("dirLight.specular", dirLightSpecular);
+
+        // view/projection transformations
+        wall1Shader.setMat4("projection", projection);
+        wall1Shader.setMat4("view", view);
+
+        // world transformation
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, wallPositions[0]);
+        model = glm::scale(model, glm::vec3(5.0f, 2.1f, 0.15f));
+        wall1Shader.setMat4("model", model);
+
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        // ------------------------------------------- 2nd wall --------------------------------------------------
+        // uses the same shader as 1st wall
+        // world transformation
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, wallPositions[1]);
+        model = glm::rotate(model, glm::radians(90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        model = glm::scale(model, glm::vec3(2.5f, 2.1f, 0.15f));
+        wall1Shader.setMat4("model", model);
+
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        // ------------------------------------------- 3rd wall --------------------------------------------------
+        wall2Shader.use();
+
+        wall2Shader.setVec3("viewPos", camera.Position);
+        wall2Shader.setFloat("material.shininess", 32.0f);
+
+        // directional light
+        wall2Shader.setVec3("dirLight.direction", direction);
+        wall2Shader.setVec3("dirLight.ambient", dirLightAmbient);
+        wall2Shader.setVec3("dirLight.diffuse", dirLightDiffuse);
+        wall2Shader.setVec3("dirLight.specular", dirLightSpecular);
+
+        // view/projection transformations
+        wall2Shader.setMat4("projection", projection);
+        wall2Shader.setMat4("view", view);
+
+        // world transformation
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, wallPositions[2]);
+        model = glm::scale(model, glm::vec3(3.0f, 2.1f, 0.15f));
+        wall2Shader.setMat4("model", model);
+
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        // ------------------------------------------- 4th wall --------------------------------------------------
+        // uses the same shader as 3rd wall
+        // world transformation
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, wallPositions[3]);
+        model = glm::rotate(model, glm::radians(90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        model = glm::scale(model, glm::vec3(4.0f, 2.1f, 0.15f));
+        wall2Shader.setMat4("model", model);
+
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        // =========================================== walls drawn ===============================================
+
+        // I don't want face culling for anything other than platforms and walls
+        glDisable(GL_CULL_FACE);
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         glfwSwapBuffers(window);
